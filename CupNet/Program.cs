0f66be466335aab25b9b5ef5f3d31f7318f3d38e
Kyota.exe <kyota.poeti@icloud.com>
2021-12-11 23:19:15 -0500

    Initial commit

---
+++ CupNet/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CupNet
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Title = "CupNet Server";
+            Server server = new Server();
+            server.Start(0, 0);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CupNet/Server.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace CupNet
+{
+    public class Server
+    {
+        public static int MaxPlayers { get; private set; }
+        public static int Port { get; private set; }
+
+        private static TcpListener tcpListener;
+
+        public void Start(int _maxPlayers, int _port)
+        {
+            MaxPlayers = _maxPlayers;
+            Port = _port;
+
+            Console.WriteLine("Starting server...");
+            
+            tcpListener = new TcpListener(IPAddress.Any, Port);
+            tcpListener.Start();
+
+            Console.WriteLine($"Server started on {Port}.");
+            
+            tcpListener.BeginAcceptTcpClient(OnTcpClientConnect, null);
+        }
+
+        private static void OnTcpClientConnect(IAsyncResult asyncResult)
+        {
+            TcpClient client = tcpListener.EndAcceptTcpClient(asyncResult);
+            tcpListener.BeginAcceptTcpClient(OnTcpClientConnect, null);
+        }
+    }
+}
